06c8468336957720eec944a30a9f1fd07ac1796c
atinea-nl <39120094+atinea-nl@users.noreply.github.com>
2023-04-25 11:26:27 +0200

    Added type input and pretty printer (#5)
    Co-authored-by: adus <adus@atinea.pl>

--- json-ptd/index.js
+++ json-ptd/index.js
@@ -21,76 +21,167 @@ const METATYPE = {
     }}
 }
 
-const success = document.querySelector('.validator-success');
-const error = document.querySelector('.validator-error');
-const internal = document.querySelector('.validator-internal');
+const MESSAGE_TYPES = {
+    "ERROR": "error",
+    "INTERNAL": "internal",
+    "SUCCESS": "success"
+};
 
-const typeTextArea = document.querySelector('#ptd-type');
+const MESSAGES = {
+    "TYPELIB_EMPTY": "TypeLib is empty!",
+    "TYPE_EMPTY": "Type is empty!",
+    "VALUE_EMPTY": "Value is empty!",
+    "TYPELIB_INTERNAL": "Internal error occured during typeLib parsing (incorrect JSON)!",
+    "VALUE_INTERNAL": "Internal error occured during value parsing (incorrect JSON)!",
+    "TYPELIB_ERROR": "TypeLib mismatch with metatype!",
+    "TYPE_ERROR": "There is no corresponding type in TypeLib!",
+    "ERROR": "The value does not correspond to the type!",
+    "SUCCESS": "The value does conforms to the type!"
+};
+
+const COLORS = {
+    "ERROR": "#f8d7da",
+    "INTERNAL": "#fff3cd",
+    "SUCCESS": "#d1e7dd"
+}
+
+const messageDiv = document.querySelector('.validator-message');
+
+const typeLibTextArea = document.querySelector('#ptd-typelib');
 const valueTextArea = document.querySelector('#ptd-value');
+const typeInput = document.querySelector('#ptd-type');
+
+function handleTypeLibChange() {
+    try {
+        const typeLib = typeLibTextArea.value;
+        if (typeLib.length == 0) return;
+        parsedTypeLib = JSON.parse(typeLib);
+        typeInput.value = Object.keys(parsedTypeLib)[0];
+    } catch {
+        return;
+    }
+}
+
+function handlePrettyPrint(isTypeLib) {
+    const typeLib = typeLibTextArea.value;
+    const value = valueTextArea.value;
+
+    if (isTypeLib) {
+        try {
+            parsedTypeLib = JSON.parse(typeLib);
+            typeLibTextArea.value = prettyPrinter(parsedTypeLib);
+        } catch {
+            showMessage(MESSAGES.TYPELIB_INTERNAL, MESSAGE_TYPES.INTERNAL);
+            typeLibTextArea.style.background = COLORS.INTERNAL;
+            return;
+        }
+    } else {
+        try {
+            parsedValue = JSON.parse(value);
+            valueTextArea.value = prettyPrinter(parsedValue);
+        } catch {
+            showMessage(MESSAGES.VALUE_INTERNAL, MESSAGE_TYPES.INTERNAL);
+            valueTextArea.style.background = COLORS.INTERNAL;
+            return;
+        }
+    }
+    clearFormatting();
+}
 
 function handleValidateClick() {
-    const type = typeTextArea.value;
+    const typeLib = typeLibTextArea.value;
     const value = valueTextArea.value;
-    
-    if (type.length === 0 || value.length === 0) {
-        showInternal();
+    const typeName = typeInput.value;
+
+    let parsedTypeLib = {};
+    let parsedValue = {};
+
+    // type checking
+    if (typeName.length == 0) {
+        showMessage(MESSAGES.TYPE_EMPTY, MESSAGE_TYPES.INTERNAL);
+        typeInput.style.background = COLORS.INTERNAL;
         return;
-    } 
+    }
+
+    // typeLib checking
+    if (typeLib.length == 0) {
+        showMessage(MESSAGES.TYPELIB_EMPTY, MESSAGE_TYPES.INTERNAL);
+        typeLibTextArea.style.background = COLORS.INTERNAL;
+        return;
+    }
+
     try {
-        const parsedValue = JSON.parse(value);
-        const parsedTypeLib = JSON.parse(type);
-        const typeName = Object.keys(parsedTypeLib)[0];
-        if (verifyType(parsedTypeLib)) {
-            verifyValue(parsedTypeLib, typeName, parsedValue);
-        }
+        parsedTypeLib = JSON.parse(typeLib);
     } catch {
-        showInternal();
+        showMessage(MESSAGES.TYPELIB_INTERNAL, MESSAGE_TYPES.INTERNAL);
+        typeLibTextArea.style.background = COLORS.INTERNAL;
+        return;
     }
-}
 
-function verifyType(typeLib) {
-    if(verify(typeLib, Object.keys(METATYPE)[0], METATYPE)) {
-        return true;
+    if (!verify(parsedTypeLib, Object.keys(METATYPE)[0], METATYPE)) {
+        showMessage(MESSAGES.TYPELIB_ERROR, MESSAGE_TYPES.ERROR);
+        typeLibTextArea.style.background = COLORS.ERROR;
+        return;
+    }
+
+    // does typeLibe contain type checking
+    if (parsedTypeLib[typeName] === undefined) {
+        showMessage(MESSAGES.TYPE_ERROR, MESSAGE_TYPES.ERROR);
+        typeLibTextArea.style.background = COLORS.ERROR;
+        typeInput.style.background = COLORS.ERROR;
+        return;
+    }
+
+    // value checking
+    if (value.length == 0) {
+        showMessage(MESSAGES.VALUE_EMPTY, MESSAGE_TYPES.INTERNAL);
+        valueTextArea.style.background = COLORS.INTERNAL;
+        return;
+    }
+
+    try {
+        parsedValue = JSON.parse(value);
+    } catch {
+        showMessage(MESSAGES.VALUE_INTERNAL, MESSAGE_TYPES.INTERNAL);
+        valueTextArea.style.background = COLORS.INTERNAL;
+        return;
+    }
+
+    // validation
+    if (verify(parsedValue, typeName, parsedTypeLib)) {
+        showMessage(MESSAGES.SUCCESS, MESSAGE_TYPES.SUCCESS);
+        typeLibTextArea.style.background = COLORS.SUCCESS;
+        typeInput.style.background = COLORS.SUCCESS;
+        valueTextArea.style.background = COLORS.SUCCESS;
     } else {
-        showError('Type mismatch with metatype!');
-        typeTextArea.style.background = '#f8d7da'
-        return false;
+        showMessage(MESSAGES.ERROR, MESSAGE_TYPES.ERROR);
+        typeLibTextArea.style.background = COLORS.ERROR;
+        typeInput.style.background = COLORS.ERROR;
+        valueTextArea.style.background = COLORS.ERROR;
     }
+    return;
 }
 
-function verifyValue(typeLib, typeName, value) {
-    if(verify(value, typeName, typeLib)) {
-        showSuccess();
-    } else {
-        showError('The value does not correspond to the type!');
-        valueTextArea.style.background = '#f8d7da'
+function showMessage(message, messageType) {
+    clearFormatting();
+    if (messageType === MESSAGE_TYPES.ERROR) {
+        messageDiv.classList.add('validator-error');
+    } else if (messageType === MESSAGE_TYPES.INTERNAL) {
+        messageDiv.classList.add('validator-internal');
+    } else if (messageType === MESSAGE_TYPES.SUCCESS) {
+        messageDiv.classList.add('validator-success');
     }
+
+    messageDiv.innerHTML = message;
+    messageDiv.style.display = 'block';   
 }
 
-function showInternal() {
+function clearFormatting () {
+    messageDiv.classList.remove('validator-error');
+    messageDiv.classList.remove('validator-internal');
+    messageDiv.classList.remove('validator-success');
     valueTextArea.style.background = '#ffffff';
-    typeTextArea.style.background = '#ffffff';
-    error.style.display = 'none';
-    success.style.display = 'none';
-    internal.classList.add('fadeIn');
-    internal.style.display = 'block';   
-}
-
-function showError(errorMessage) {
-    valueTextArea.style.background = '#ffffff';
-    typeTextArea.style.background = '#ffffff';
-    internal.style.display = 'none';
-    success.style.display = 'none';  
-    error.innerHTML = errorMessage;
-    error.classList.add('fadeIn');
-    error.style.display = 'block';
-}
-
-function showSuccess() {
-    valueTextArea.style.background = '#ffffff';
-    typeTextArea.style.background = '#ffffff';
-    error.style.display = 'none';
-    internal.style.display = 'none';   
-    success.classList.add('fadeIn');
-    success.style.display = 'block';
+    typeLibTextArea.style.background = '#ffffff';
+    typeInput.style.background = '#ffffff';
+    messageDiv.style.display = 'none';
 }
